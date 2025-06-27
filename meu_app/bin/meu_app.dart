984e3cb6db4114114252a35fc5ec4b3e824cb01f
DaviRSuassuna <davisuassuna@unitins.br>
2025-06-27 15:40:50 -0300

    version_7

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -218,3 +218,66 @@
 //   print(map1);
 //   print(map1["Idade"]);
 // }
+
+// void main(List<String> arguments) {
+//   const String variavel1 = "ABC";
+//   const int variavel2 = 1;
+//   const bool variavel3 = true;
+
+//   print(variavel1);
+//   print(variavel2);
+//   print(variavel3);
+
+//   //variavel = "CDE";
+// }
+
+// void main(List<String> arguments) {
+//   dynamic var1 = "ABC";
+//   var var2 = "ABC";
+
+//   print(var1.length);
+//   print(var2.length);
+
+//   var1 = 10;
+//   print(var1);
+
+//   var1 = 99.54;
+//   print(var1);
+
+//   var1 = true;
+//   print(var1);
+
+//   var1 = DateTime(2022, 01, 02);
+//   print(var1);
+
+//   var1 = [10, true, "A"];
+//   print(var1);
+// }
+
+void main(List<String> arguments) {
+  DateTime data1 = DateTime.now();
+  // data1 = DateTime.parse("2025-06-27 15:30:00");
+
+  print(data1);
+  print(data1.day);
+  print(data1.month);
+  print(data1.year);
+  print(data1.hour);
+  print(data1.minute);
+  print(data1.second);
+
+  print(data1.weekday);
+
+  print(data1);
+  data1 = data1.add(Duration(days: 1));
+  print(data1.add(Duration(days: 1)));
+  print(data1);
+
+  print(data1.subtract(Duration(hours: 1)));
+  print(data1);
+
+  var data2 = DateTime.parse("2022-02-01 00:00:00");
+  print(data1.isAfter(data2));
+  print(data1.isBefore(data2));
+  print(data1.compareTo(data2));
+}
